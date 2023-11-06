06bd2384edb2fa8baa768e66662ba673f854da54
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-11-06 03:18:52 +0000

    [Start]app2-24

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -11,3 +11,13 @@ environment.plugins.prepend('Provide',
 )
 
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
